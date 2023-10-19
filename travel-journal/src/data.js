96b3867439c0d967431948f4b9c8622338086d65
Teodor Agape <teodor.agape@yahoo.com>
2023-10-19 15:39:38 +0200

    new travels

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -32,4 +32,68 @@ export default [
       "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
     imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
   },
+  {
+    title: "Yosemite Valley",
+    location: "Yosemite National Park",
+    googleMapsUrl: "https://maps.app.goo.gl/LJV7sN82zbv9pQ2n6",
+    startDate: "12 Jun 2023",
+    endDate: "16 Jun 2023",
+    description:
+      "Yosemite Valley is a glacial valley in California's Sierra Nevada mountains, celebrated for its iconic granite cliffs, waterfalls, and unique biodiversity.",
+    imageUrl:
+      "https://images.squarespace-cdn.com/content/v1/596b2969d2b85786e6892853/1581141131829-AUCS3CET0IMY9D3K8FJ4/Yosemite+Photography+Spots+-+Taft+Point?format=2500w",
+    alt: "Half Dome",
+    id: 4,
+  },
+  {
+    title: "Milford Sound",
+    location: "New Zealand",
+    googleMapsUrl: "https://maps.app.goo.gl/wq1uQTaTwn97EMAj9",
+    startDate: "10 Mar, 2023",
+    endDate: "17 Mar, 2023",
+    description:
+      "Milford Sound is a breathtaking fjord in New Zealand's South Island, renowned for its dramatic cliffs, cascading waterfalls, and stunning natural beauty.",
+    imageUrl:
+      "https://beaumicheli.files.wordpress.com/2020/03/milford-sound-portrait.jpg?w=811",
+    alt: "Milford Sound",
+    id: 5,
+  },
+  {
+    title: "Cataratas del Iguazú",
+    location: "Argentina and Brazil",
+    googleMapsUrl: "https://maps.app.goo.gl/9pdLBvenfVcNVw9w5",
+    startDate: "21 Mar, 2019",
+    endDate: "22 Mar, 2019",
+    description:
+      "Iguazú Falls is a colossal system of waterfalls straddling the border of Brazil and Argentina, renowned for its awe-inspiring beauty and power.",
+    imageUrl:
+      "https://img.freepik.com/premium-photo/iguazu-falls-national-park-brazil-waterfall-beautiful-view_289152-44.jpg?w=2000",
+    alt: "Iguazú Falls",
+    id: 6,
+  },
+  {
+    title: "Salzburg",
+    location: "Austria",
+    googleMapsUrl: "https://maps.app.goo.gl/hFyuaksb2L2W6pDy7",
+    startDate: "May 6, 2016",
+    endDate: "May 7, 2016",
+    description:
+      "Salzburg is an Austrian city known for its baroque architecture, historic birthplace of Mozart, and the picturesque backdrop of the Alps.",
+    imageUrl:
+      "https://www.travelsbyknutte.com/wp-content/uploads/2020/04/DSC5149-684x1024.jpg",
+    alt: "Salzburg",
+    id: 7,
+  },
+  {
+    title: "Efes",
+    location: "Turkey",
+    googleMapsUrl: "https://maps.app.goo.gl/q5qEjvgSN9Tdf1CXA",
+    startDate: "May 2, 2014",
+    endDate: "May 4, 2014",
+    description:
+      "Ephesus is an ancient Greek city in modern-day Turkey, renowned for its grand ruins, including the Temple of Artemis, one of the Seven Wonders of the Ancient World.",
+    imageUrl: "https://getsetandgo.files.wordpress.com/2020/01/dscn5264.jpg",
+    alt: "Library of Celsus",
+    id: 8,
+  },
 ];
